a30415f09b05d53aa0d8331983d859038b4eeda3
jultdev <andikaraeng32@gmail.com>
2022-04-03 21:54:10 +0800

    admin done

--- app/Controllers/Admin.php
+++ app/Controllers/Admin.php
@@ -68,4 +68,59 @@ class Admin extends BaseController
                 ->with('message', 'Password tidak sama');
         }
     }
+
+    public function edit_admin($id)
+    {
+        $admin = new M_admin();
+
+        $data = [
+            'title' => 'Edit Admin'
+        ];
+
+        $data['admin'] = $admin->find($id);
+
+        return view('admin/edit-admin', $data);
+    }
+
+    public function update_admin($id)
+    {
+        $admin = new M_admin();
+
+        $img = (null !== $this->request->getFile('gambar')) ? $this->request->getFile('gambar') : null;
+
+        $passConfirm = $this->request->getPost('confirm');
+
+        if ($this->request->getPost('password') == $passConfirm) {
+
+            $data = [
+                'fullname' => $this->request->getPost('fullname'),
+                'username' => $this->request->getPost('username'),
+                'password' => password_hash($this->request->getPost('password'), PASSWORD_DEFAULT),
+                'type' => $this->request->getPost('jenis'),
+                'gambar' => $if = (isset($img)) ? $img->getName() : 'avatar5.png'
+            ];
+
+            if (isset($img) && $img->isValid() && !$img->hasMoved()) {
+                $img->move(base_url('admin/uploads/'));
+            }
+
+            $admin->update($id, $data);
+
+            return redirect()->to(base_url('m-admin/admin'))->with('type-status', 'success')
+                ->with('message', 'Admin baru telah ditembahkan');
+        } else {
+
+            return redirect()->to(base_url('m-admin/admin/edit' . $id))->with('type-status', 'error')
+                ->with('message', 'Password tidak sama');
+        }
+    }
+
+    public function delete_admin($id)
+    {
+        $admin = new M_admin();
+
+        $admin->delete($id);
+        return redirect()->to(base_url('m-admin/admin'))->with('type-status', 'success')
+            ->with('message', `Admin dengan ID $id telah dihapus`);
+    }
 }
---
+++ app/Views/admin/edit-admin.php
@@ -0,0 +1,90 @@
+<?= $this->extend('admin/base') ?>
+<?= $this->section('content') ?>
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <a href="<?= base_url('m-admin/admin') ?>" class="btn btn-block btn-primary btn-flat">Kembali</a>
+                </div><!-- /.col -->
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item">Dashboard</li>
+                        <li class="breadcrumb-item">Table Admin</li>
+                        <li class="breadcrumb-item active">Edit Data Admin</li>
+                    </ol>
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <div class="content">
+        <div class="container-fluid">
+            <form action="<?= base_url('m-admin/admin/update') ?>" method="post" enctype="multipart/form-data">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="card card-primary">
+                            <div class="card-header">
+                                <h3 class="card-title">Upload Gambar</h3>
+                            </div>
+                            <div class="card-body">
+                                <input type="file" name="gambar" id="gambar">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card card-blue">
+                            <div class="card-header">
+                                <h3 class="card-title">Form Input Data Admin</h3>
+                            </div>
+                            <div class="card-body">
+                                <div class="form-group">
+                                    <label for="fullname">Fullname</label>
+                                    <input type="text" name="fullname" id="fullname" class="form-control"
+                                        value="<?= $admin['fullname'] ?>">
+                                </div>
+                                <div class="form-group">
+                                    <label for="username">Username</label>
+                                    <input type="text" name="username" id="username" class="form-control"
+                                        value="<?= $admin['username'] ?>">
+                                </div>
+                                <div class="form-group">
+                                    <label for="jenis">Jenis</label>
+                                    <select name="jenis" id="jenis" class="form-control">
+                                        <option value="pegawai"
+                                            <?= $select = ($admin['type'] == 'pegawai') ? 'Selected' : '' ?>>
+                                            Pegawai
+                                        </option>
+                                    </select>
+                                </div>
+                                <div class="form-group">
+                                    <label for="password">Password</label>
+                                    <input type="password" name="password" id="password" class="form-control"
+                                        value='<?= $admin['password'] ?>'>
+                                </div>
+                                <div class="form-group">
+                                    <label for="confirm">Konfirmasi Password</label>
+                                    <input type="password" name="confirm" id="confirm" class="form-control"
+                                        value="<?= $admin['password'] ?>">
+                                </div>
+                            </div>
+                            <div class="card-footer">
+                                <div class="row">
+                                    <div class="col-10"></div>
+                                    <div class="col-2"><button class="btn btn-block btn-primary btn-flat"
+                                            type="submit">Ubah</button></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </form>
+            <!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content -->
+</div>
+<?= $this->endSection() ?>
